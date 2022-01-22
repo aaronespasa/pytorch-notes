b742c29525bde413f93fb41c35f43e95df68110f
aaronespasa <aaespasa@gmail.com>
2022-01-22 16:34:42 +0100

    Adding weight initialization

--- models/SimpleCNN.py
+++ models/SimpleCNN.py
@@ -36,3 +36,14 @@ class CNN(nn.Module):
 
         return x
 
+    def initialize_weights(self):
+        # Weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight)
+
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
